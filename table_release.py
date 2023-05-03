9dbfc27786a7174e76c00806fb63050a8ff2dbec
Giyasgm <118182647+Giyasgm@users.noreply.github.com>
2023-05-03 15:51:43 +0300

    Экранировал символы

--- table_release.py
+++ table_release.py
@@ -21,27 +21,27 @@ try:
                 print(f"Имя - {i[1]}, возраст - {i[2]}, рост - {i[3]}, цвет волос - {i[4]}, размер груди - {i[5]}, номер в таблице - {i[0]}")
 
     def defname(name):
-        sql_request = f"""SELECT * FROM anime WHERE name = '{name}';"""
+        sql_request = f"""SELECT * FROM anime WHERE name = \'{name}\';"""
         print_waifus_from_db(sql_request)
         
     def defage(age):
-        sql_request = f"""SELECT * FROM anime WHERE age <= '{age}';"""
+        sql_request = f"""SELECT * FROM anime WHERE age <= \'{age}\';"""
         print_waifus_from_db(sql_request)
 
     def defheight(height):
-        sql_request = f"""SELECT * FROM anime WHERE eight <= '{height}';"""
+        sql_request = f"""SELECT * FROM anime WHERE eight <= \'{height}\';"""
         print_waifus_from_db(sql_request)
 
     def defhair(hair):
-        sql_request = f"""SELECT * FROM anime WHERE hair_color = '{hair}';"""
+        sql_request = f"""SELECT * FROM anime WHERE hair_color = \'{hair}\';"""
         print_waifus_from_db(sql_request)
 
     def defsize(size):
-        sql_request = f"""SELECT * FROM anime WHERE breast_size = '{size}';"""
+        sql_request = f"""SELECT * FROM anime WHERE breast_size = \'{size}\';"""
         print_waifus_from_db(sql_request)
     
 
-    sfx = input("Введите один из перечисленных параметров: name, age, height, hair_color, breast_size - ")
+    sfx = input("Введите один из перечисленных параметров: name, age, height, hair_color, breast_size - ").lower()
     
     if sfx == "name":
         print("Выберите одно из имён: ")
